dae1dd0c42a80ca9f1502da84ce647cacd5ad35a
Susara Withanage <134674490+susara-withanage@users.noreply.github.com>
2023-05-31 12:21:50 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -147,7 +147,7 @@ function generateNotification() {
   const image = document.createElement("img");
   image.alt = "Notification";
   
-  image.src = "resources/notification/14.jpg"; // imageSources[currentIndex]; // Set the source from the array
+  image.src = imageSources[currentIndex]; // Set the source from the array
   currentIndex = (currentIndex + 1) % imageSources.length; // Increment index and wrap around
   
   image.addEventListener("load", () => {
